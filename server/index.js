4b2324e9ca910449f11610c9830ec832e856902e
Ghostdanish <balochdanish2020@gmail.com>
2022-11-16 22:14:32 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+//New imports
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const socketIO = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+let users = [];
+
+//Add this before the app.get() block
+socketIO.on("connection", (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    //sends the message to all the users on the server
+    socket.on("message", (data) => {
+        console.log(data);
+        socketIO.emit("messageResponse", data);
+    });
+
+    //Listens when a new user joins the server
+    socket.on("newUser", (data) => {
+        //Adds the new user to the list of users
+        users.push(data);
+        // console.log(users);
+        //Sends the list of users to the client
+        socketIO.emit("newUserResponse", users);
+    });
+
+    socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
+
+    socket.on("disconnect", () => {
+        console.log("🔥: A user disconnected");
+        //Updates the list of users when a user disconnects from the server
+        users = users.filter((user) => user.socketID !== socket.id);
+        // console.log(users);
+        //Sends the list of users to the client
+        socketIO.emit("newUserResponse", users);
+        socket.disconnect();
+    });
+});
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Hello world",
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
